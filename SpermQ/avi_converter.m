ef37a261b6ff064d0e8f39e87c8a930d45b8e36d
Crispian Morris <60779224+wg19671@users.noreply.github.com>
2021-02-11 10:25:34 +0000

    Added SpermQ instructions and some necessary files and links, plus MATLAB code to convert .avi files to .tif stacks

---
+++ SpermQ/avi_converter.m
@@ -0,0 +1,12 @@
+obj = VideoReader('obj 3 phase 20x dia 92 1x zoom no binning Rabbit delta 78 heterozygote022.avi');
+vid = read(obj);
+frames = obj.NumFrames;
+
+for x = 1 : frames
+    imwrite(vid(:,:,:,x),strcat('frame- ',num2str(1),'.tif'), 'Compression', 'none', 'WriteMode', 'append');
+end
+
+im2gray(strcat('frame- ',num2str(1),'.tif'))
+
+im2gray(imread('frame-1.tif'));
+im2gray(imread('frame-1_SpQP.tif'));
