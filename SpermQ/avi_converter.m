9d9a6d7b18264b94bb13dd6df4a90ea035b7bc43
Crispian Morris <60779224+wg19671@users.noreply.github.com>
2021-02-15 21:29:13 +0000

    added resulting files for Rabbit from SpermQ

--- SpermQ/avi_converter.m
+++
@@ -1,7 +0,0 @@
-obj = VideoReader('obj 3 phase 20x dia 92 1x zoom no binning Rabbit delta 78 heterozygote022.avi');
-vid = read(obj);
-frames = obj.NumFrames;
-
-for x = 1 : frames
-    imwrite(vid(:,:,:,x),strcat('frame- ',num2str(1),'.tif'), 'Compression', 'none', 'WriteMode', 'append');
-end
